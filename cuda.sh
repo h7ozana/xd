242b32dd8d53e9e4ecca1aeaba0d0df301467a28
h7ozana <154946538+h7ozana@users.noreply.github.com>
2024-11-21 11:08:53 +0900

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
 sudo apt -y install cuda-toolkit-12-2
