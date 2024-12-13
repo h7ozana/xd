c211ef2d0ec0ae40f9c68d185777795928070401
h7ozana <h7ozaza@gmail.com>
2024-12-13 14:47:20 +0900

    compose

--- docker.sh
+++ docker.sh
@@ -6,3 +6,9 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
 sudo apt update
 sudo apt install nvidia-docker2 -y
 sudo systemctl restart docker
+
+
+#compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version #버전 확인
