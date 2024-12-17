9f29cfe0bc94b3f3391c8897922977db1815f0bc
h7ozana <h7ozaza@gmail.com>
2024-12-17 17:48:40 +0900

    his

--- cuda.sh
+++ cuda.sh
@@ -7,4 +7,5 @@ echo "export PATH=/usr/local/cuda-12.2/bin${PATH:+:${PATH}}" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
 # sudo reboot
 sudo apt -y install cudnn
+history -c
 sudo reboot
