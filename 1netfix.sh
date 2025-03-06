3fb8d768a35b2e8dae4c3524b7d14208bad1235a
h7ozana <h7ozaza@gmail.com>
2025-03-06 14:44:34 +0900

    1net

---
+++ 1netfix.sh
@@ -0,0 +1,3 @@
+sudo mkdir -p /etc/cloud/cloud.cfg.d/ && echo "network: {config: disabled}" | sudo tee -a /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+sudo cloud-init clean
+sudo netplan apply
