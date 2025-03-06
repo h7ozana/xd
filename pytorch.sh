80d46614f51a3655917ea20ad53228ed9488cb46
h7ozana <h7ozaza@gmail.com>
2025-03-06 15:59:38 +0900

    3

--- pytorch.sh
+++ pytorch.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
-sudo apt install -y python3 python3-pip python3.10-venv ghostscript
+sudo apt install -y python3 python3-pip
 
-python3 -m venv test1
-source test1/bin/activate
+pip3 install torch torchvision torchaudio
 
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip3 install matplotlib progress
-cd pytorch-classification
+python3 --version
 
-machine_name="xd"
-sudo mkdir -p checkpoints/cifar10
-#for i in 0 2 4 /// for {0..1} = {0~1}
-for i in 1
-do
-  python cifar_new.py -a vgg19_bn --gpu-id $i --epochs 7 --schedule 3 5 --gamma 0.1 --workers 2 --checkpoint checkpoints/cifar10/vgg19_bn_${machine_name}_gpu$i
-done
+#동작확인 / 토치 쿠다버전
+python3 -c "import torch; print(torch.__version__)"
+python3 -c "import torch; print(torch.version.cuda)"
 
-# H100 기준 epochs 7 / schedule 3 5 >> 1분미만 vgg19_bn
-
-
-# 타 모델 변경시 모델명만 기입하고 뎁스에 레이어를 입력하는 구조
-# for i in 0
-# do
-#   python cifar_new.py -a preresnet --depth 110 --gpu-id $i --epochs 7 --schedule 3 5 --gamma 0.1 --workers 2 --checkpoint checkpoints/cifar10/preresnet_${machine_name}_gpu$i
-# done
-
-
-
-#오류시 고스트삭제
-
-
-# cd ~/pytorch-classification/checkpoints/cifar10/vgg19_bn_xd_gpu0
-# sudo gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=output.pdf log.eps
-# git remote add origin http://github.com/h7ozana/data
-
-#윈도우에서 파일 떠오기
-# 예시 scp user@<서버_IP>:~/pytorch-classification/checkpoints/cifar10/vgg19_bn_xd_gpu0/output.pdf C:\Users\<사용자명>\Desktop\
-# scp user@192.168.39.39:~/pytorch-classification/checkpoints/cifar10/vgg19_bn_xd_gpu0/output.pdf C:\Users\user\Desktop\
+#GPU확인
+python3 -c "import torch; print(f'Total GPUs: {torch.cuda.device_count()}'); print([torch.cuda.get_device_name(i) for i in range(torch.cuda.device_count())])"
+python3 -c "import torch; print([torch.cuda.is_available() for i in range(torch.cuda.device_count())])"
