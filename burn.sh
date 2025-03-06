23d395f0744238700655b5fd6be904ab284d3ab4
h7ozana <h7ozaza@gmail.com>
2025-03-06 14:50:34 +0900

    make

--- burn.sh
+++ burn.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo git clone https://github.com/wilicc/gpu-burn
 cd gpu-burn
-sudo docker build -t gpu_burn .
-cd ..
-sudo rm -rf gpu-burn
-# docker run --rm --gpus all gpu_burn
-# sudo docker run -it --gpus '"device=0"' gpu_burn  /bin/bash
+make
+
+# sudo docker build -t gpu_burn .
+# cd ..
+# sudo rm -rf gpu-burn
+# # docker run --rm --gpus all gpu_burn
+# # sudo docker run -it --gpus '"device=0"' gpu_burn  /bin/bash
